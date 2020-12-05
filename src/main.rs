c8926fea3338bd3c28865f6e775718ecb33a0013
xvi <jrip03@gmail.com>
2020-12-05 18:28:15 -0500

    fixed err with Result to std::result::Result

--- src/main.rs
+++ src/main.rs
@@ -26,16 +26,19 @@ use hyper_tls::HttpsConnector;
 use tokio;
 use dotenv::dotenv;
 
-// Here's where the magic happens...
 #[tokio::main]
-async fn main() -> Result<()/*, Box<dyn std::error::Error + Send + Sync>*/> {
+async fn main() -> std::result::Result<
+    (), // this kinda hard to read mb
+    Box<dyn std::error::Error + Send + Sync>
+    > {
     //  why do you have the habit of checking dotfiles for no reason???
     dotenv().ok();
     
     // Making an https client
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(https);
-    let target_uri: hyper::Uri = env::var("shape_site_url").parse()?;  
+    // let target_uri: hyper::Uri = env::var("shape_site_url").unwrap().parse()?;  
+    let target_uri: hyper::Uri = "https://assets.targetimg1.com/ssx/ssx.mod.js".parse()?;
         
     let mut counter: u8 = 0;
     loop{
